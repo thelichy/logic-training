8b79228d8127b75b2964fcf14d2fc1d2baa5aa6a
Thierry <thierryalefh77@gmail.com>
2023-06-16 15:11:45 -0300

    binary search with Ordering

--- binary_search/src/main.rs
+++ binary_search/src/main.rs
@@ -1,30 +1,32 @@
-use std::cmp::PartialOrd;
-
+use std::cmp::{ Ord, Ordering };
 
 
 fn main() {
     let list = vec![1, 2, 3, 5, 6, 7, 9, 20, 30, 54];
-    let item = 6;
+    let item = 7;
 
     match binary_search(&list, item, 0, list.len(), None) {
         Some(r) => println!("{} found at: {}", item, r),
-        None => println!("{} was not found", item)
+        None => println!("{} was not found", item),
     };
 }
 
-
-fn binary_search<T: PartialOrd>(list: &[T], item: T, min: usize, max: usize, last: Option<usize>) -> Option<usize> {
+fn binary_search<T: Ord>(
+    list: &[T],
+    item: T,
+    min: usize,
+    max: usize,
+    last: Option<usize>
+) -> Option<usize> {
     let mid: usize = (max + min) / 2;
 
     if min > max || max < min || mid == last.unwrap_or(list.len()) {
         return None;
     }
 
-    if item > list[mid] {
-        return binary_search::<T>(&list, item, mid, max, Some(mid));
-    } else if item < list[mid] {
-        return binary_search::<T>(&list, item, min, mid, Some(mid));
-    } else {
-        return Some(mid);
+    match list[mid].cmp(&item) {
+        Ordering::Equal => Some(mid),
+        Ordering::Greater => binary_search::<T>(&list, item, min, mid, Some(mid)),
+        Ordering::Less => binary_search::<T>(&list, item, mid, max, Some(mid)),
     }
 }
