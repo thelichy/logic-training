faf6404df61b521a092937b51927f318cb45f1ec
Alefh <thierryalefh77@gmail.com>
2023-09-08 18:36:18 -0300

    two sum redone

--- two_sum/main.js
+++ two_sum/main.js
@@ -1,32 +1,11 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
 
-var twoSum = (nums, target) => {
-    for (let i = 0, max = nums.length; i < max; i++) {
-        for (let j = i + 1; j < max; j++) {
-            if (nums[i] + nums[j] == target) {
-                return new Array(i, j);
-            }
+function twoSum(numbers, target) {
+    for (let i = 0; i < numbers.length - 1; i++) {
+      for (let j = i + 1; j < numbers.length; j++) {
+        if (i != j && numbers[i] + numbers[j] == target) {
+          return [i, j];
         }
-    }
-
-    return new Array(-1, -1);
-
-};
-
-
-twoSum = (nums, target) => { // ! SLOWER
-    for (let i = 0, max = nums.length; i < max; i++) {
-        let f = nums.findIndex(n => n == target - nums[i]);
-        if (i == f) continue;
-
-        if (nums[i] + nums[f] == target) {
-            return new Array(i, f);
-        }
-    }
-
-    return new Array(-1, -1);
-};
+      }
+    }  
+  }
+  
